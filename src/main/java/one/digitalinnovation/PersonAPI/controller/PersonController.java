4137771294622141448cb089758dda9b18471cd0
MoaresOliveira <moaresoliveira@gmail.com>
2021-08-03 00:53:18 -0300

    Repositorio e Controller da classe Person criados, e swagger implementado

---
+++ src/main/java/one/digitalinnovation/PersonAPI/controller/PersonController.java
@@ -0,0 +1,31 @@
+package one.digitalinnovation.PersonAPI.controller;
+
+import one.digitalinnovation.PersonAPI.dto.MessageResponseDTO;
+import one.digitalinnovation.PersonAPI.entity.Person;
+import one.digitalinnovation.PersonAPI.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/person")
+public class PersonController {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+}
