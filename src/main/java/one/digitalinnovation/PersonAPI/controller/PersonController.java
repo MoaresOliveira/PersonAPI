8412563248bcfe0a3834e7e8365776fe71b3c05f
MoaresOliveira <moaresoliveira@gmail.com>
2021-08-03 10:53:18 -0300

    Classe PersonService criada

--- src/main/java/one/digitalinnovation/PersonAPI/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/PersonAPI/controller/PersonController.java
@@ -2,30 +2,25 @@ package one.digitalinnovation.PersonAPI.controller;
 
 import one.digitalinnovation.PersonAPI.dto.MessageResponseDTO;
 import one.digitalinnovation.PersonAPI.entity.Person;
-import one.digitalinnovation.PersonAPI.repository.PersonRepository;
+import one.digitalinnovation.PersonAPI.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/person")
 public class PersonController {
 
-    private PersonRepository personRepository;
+    private PersonService personService;
 
     @Autowired
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody Person person){
-        Person savedPerson = personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID " + savedPerson.getId())
-                .build();
+        return personService.create(person);
     }
 }
---
+++ src/main/java/one/digitalinnovation/PersonAPI/service/PersonService.java
@@ -0,0 +1,29 @@
+package one.digitalinnovation.PersonAPI.service;
+
+import one.digitalinnovation.PersonAPI.dto.MessageResponseDTO;
+import one.digitalinnovation.PersonAPI.entity.Person;
+import one.digitalinnovation.PersonAPI.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+
+    public MessageResponseDTO create(@RequestBody Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+}
